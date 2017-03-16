e0ad4ab2bfa894c8cce20f7078f0f89ad4409cb0
rozytwan <rozy.twanju@krennova.com.np>
2017-03-16 15:02:32 +0545

    Database First Apporach

---
+++ Limited/Controllers/UploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Limited.Models;
+
+namespace Limited.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View("Create");
+        }
+
+      
+
+        TestDBEntities db = new TestDBEntities();
+        public ActionResult Create(Contact newContact)
+        {
+            try
+            {
+                Contact objContact = new Contact();
+                objContact.Address = newContact.Address;
+                objContact.Contact_Number = newContact.Contact_Number;
+                objContact.Name = newContact.Name;
+                db.Contacts.Add(objContact);
+                db.SaveChanges();
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return View();
+        }
+    }
+
+}
---
+++ Limited/Models/DownloadFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Limited.Models
+{
+    public class DownloadFile
+    {
+        public int FileId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+
+
+    }
+}
